e524b6b2a9ec1464703b037e71b98abe5168027b
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-17 18:30:44 +0530

    custom hook to get data

---
+++ src/components/Usefetch.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { Data } from "./WeatherDetails";
+export type Error={
+    message: string;
+  
+}
+
+const UseFetch = (city:string) => {
+  const [data, setData] = useState<Data|null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error|null>(null);
+  
+  useEffect(() => {
+    setLoading(true);
+    setData(null);
+    setError(null);
+    
+    
+
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+          );
+
+        if (!response.ok) {
+          throw new Error("could not fetch city");
+        }
+
+        const resData = await response.json();
+        setData(resData);
+        setLoading(false);
+      } catch (error:any) {
+         setError(error)
+       
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [city]);
+
+  return { data, loading, error };
+};
+export default UseFetch;
