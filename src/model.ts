c921e97fa23bb6c5fd70539c49d752092d201bf8
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-18 18:22:19 +0530

    Create model.ts

---
+++ src/model.ts
@@ -0,0 +1,34 @@
+export type Favourite ={
+    name:string;
+    id:number;
+    temp:number;
+    temp_max:number;
+    temp_min:number;
+  
+  }
+
+
+  export type Data={
+    name:string;
+    id:number;
+    main:{temp:number,temp_min:number,temp_max:number}
+    weather:[{description:string, icon:string}]
+
+
+}
+
+
+export type Props={
+    data:Data;
+    favouriteCity:Favourite[]
+    setfavouriteCity:React.Dispatch<React.SetStateAction<Favourite[]>>
+}
+
+export type Error={
+    message: string;
+  
+}
+
+export type ErrorProps={
+    error:Error
+  }
